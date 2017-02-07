2e09477ebb6c5de7adc9add2a9bad09221e294aa
Renjith R <renjith.r@experionglobal.com>
2017-02-07 18:24:41 +0530

    understanding functions

--- myScript.js
+++ myScript.js
@@ -1,10 +1,22 @@
-var person={
+var person = {
 	firstname: "Renjith",
-	lastname: "R"
+	lastname: "R",
+	fullname: function(){
+		return this.firstname+" "+this.lastname;
+	}
 };
 
-var x=10,y=5,z="text";
+var x=10,y=5,z="qwe";
 
 var flag=true;
 
 var result;
+
+function myfn(){
+	document.getElementById("result").value=
+	sum(document.getElementById("n1").value,document.getElementById("n2").value);
+}
+
+function sum(p1,p2) {
+    return Number(p1)+Number(p2);
+}
